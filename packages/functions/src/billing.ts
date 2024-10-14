56b0b3ee846f20da11b94d1d730b389546a9f099
NewGin <it@harrisfederal.com>
2024-10-14 16:46:22 -0400

    Adding a billing API

---
+++ packages/functions/src/billing.ts
@@ -0,0 +1,25 @@
+import { Billing } from "@notes/core/billing";
+import { Util } from "@notes/core/util";
+import { Resource } from "sst";
+import Stripe from "stripe";
+
+export const main = Util.handler(async (event) => {
+  const { storage, source } = JSON.parse(event.body || "{}");
+  const amount = Billing.compute(storage);
+  const description = "Scratch charge";
+
+  const stripe = new Stripe(
+    // Load our secret key
+    Resource.StripeSecretKey.value,
+    { apiVersion: "2024-09-30.acacia" }
+  );
+
+  await stripe.charges.create({
+    source,
+    amount,
+    description,
+    currency: "usd",
+  });
+
+  return JSON.stringify({ status: true });
+});
