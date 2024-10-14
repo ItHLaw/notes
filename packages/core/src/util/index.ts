38ea654430425891c05fa0ac205d23387d5cd084
NewGin <it@harrisfederal.com>
2024-10-14 17:07:24 -0400

    Enabling CORS

--- packages/core/src/util/index.ts
+++ packages/core/src/util/index.ts
@@ -21,7 +21,10 @@ export module Util {
       // Return HTTP response
       return {
         body,
-        statusCode,
+        statusCode,  headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        }
       };
     };
   }
